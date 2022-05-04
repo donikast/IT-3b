b7965dd7594e8bf2dc75db84f634a1e40552ac33
MYPC <MYPC@DESKTOP-3USL5KS>
2022-05-04 21:33:24 +0300

    Упражнение филтри

--- it_2022/src/main/java/controllers/RegistrationServlet.java
+++ it_2022/src/main/java/controllers/RegistrationServlet.java
@@ -13,6 +13,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import models.Skill;
 import models.User;
 import repositories.Repository;
@@ -29,7 +30,10 @@ public class RegistrationServlet extends HttpServlet {
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+			
+		RequestDispatcher rd = request.getRequestDispatcher("/RegistrationPage.jsp");
+		rd.forward(request, response);
+	 
 	}
 
 
